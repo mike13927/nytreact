a11edcd5f33cbbe08cf4e9885abd040586f512be
Mike McIntosh <m_mci@hotmail.com>
2018-07-23 22:29:44 -0700

    Awesome

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,24 @@
 import React from "react";
+import { connect } from "react-redux";
+import { NavLink, withRouter } from "react-router-dom";
 
-const Home = () => <p>Hello!</p>;
+let Home = ({ isAuthed, dispatch, history, userInfo }) => {
+  if (isAuthed) {
+    return (
+          <p>Hello!!!!</p>
+    )
+  }
+  else {
+    return (
+      <p>Please login</p>
+    )
+  }
+}
 
-export default Home;
+const mapStateToProps = state => {
+  return state;
+};
+
+Home = connect(mapStateToProps, null)(Home);
+
+export default withRouter(Home);
